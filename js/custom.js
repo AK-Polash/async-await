e6777876dbf882a1fb8f3333ec56ed2f6d36eec1
AK-Polash <polashk199@gmail.com>
2022-11-13 03:47:35 +0600

    async & await added here

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,61 @@
-console.log("hi");
+/* ===================================================
+                    Async & Await
+=================================================== */
+let payment = true;
+let marks = 80;
+
+function one() {
+  console.log("A");
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (payment) {
+        resolve("B");
+      } else {
+        reject("Error of B");
+      }
+    }, 1000);
+  });
+  return myPromise;
+}
+
+function two() {
+  console.log("C");
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (marks >= 70 && marks <= 100) {
+        resolve("D");
+      } else {
+        reject("Error of D");
+      }
+    }, 1000);
+  });
+  return myPromise;
+}
+
+function three() {
+  console.log("E");
+  let myPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("F");
+    }, 1000);
+  });
+  return myPromise;
+}
+
+// Consuming the Code:
+async function main() {
+  try {
+    let messOne = await one();
+    console.log(messOne);
+
+    let messTwo = await two();
+    console.log(messTwo);
+
+    let messThree = await three();
+    console.log(messThree);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
